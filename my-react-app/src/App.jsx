8c22eadc02a957ec2458e4c3bac33272670eaa99
Vasil <tomov.tomov09@gmail.com>
2025-04-03 14:52:54 +0200

    started learning again react

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,24 @@
+import Header from "./Header.jsx"
+import Footer from "./Footer.jsx"
+import Food from "./Food.jsx"
+
+
+function App() {
+  return(
+    <>
+      <Header>
+      
+      </Header>
+      <Food>
+
+      </Food>
+      <Footer>
+
+      </Footer>
+    </>
+    
+  )
+
+}
+
+export default App
---
+++ my-react-app/src/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer(){
+    return(
+        <footer>
+            <p>{new Date().getFullYear()}Yourn website name</p>
+        </footer>
+    )
+}
+export default Footer
