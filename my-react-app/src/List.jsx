0d98d928fc6f214c38660def8c76deb2c7e560bf
Vasil <tomov.tomov09@gmail.com>
2025-04-04 22:08:01 +0200

    learned about clickEvent

--- my-react-app/src/List.jsx
+++
@@ -1,9 +0,0 @@
-
-function List(){
-    const fruits = ["applse", "orange", "banana", "cocnut", "pineapple"];
-
-    const listItems = fruits.map(fruit => <li>{fruit}</li>)
-
-    return(<uL>{listItems}</uL>);
-}
-export default List
