510997c1ed25fa71d02feae3835b9cc6149e2611
Vasil <tomov.tomov09@gmail.com>
2025-04-05 17:15:07 +0200

    still practicing

---
+++ my-react-app/src/ColorPicker.jsx
@@ -0,0 +1,23 @@
+import React, {useState} from 'react';
+function ColorPicker(){
+
+    const[color, setColor] = useState("#ffffff");
+
+    function handleColorChange(e){
+        setColor(e.target.value)
+
+    }
+
+    return(
+        <div className='color-picker-container'>
+            <h1>Color Picker</h1>
+            <div className="color-dispaly" style={{backgroundColor: color}}>
+                <p>Selecter Color: {color}</p>
+            </div>
+            <label>Select a Color</label>
+            <input type='color' value={color} onChange={handleColorChange}/>
+        </div>
+    )
+
+}
+export default ColorPicker
---
+++ my-react-app/src/Updater.jsx
@@ -0,0 +1,12 @@
+import React, {useState} from "react";
+function Updater(){
+    const [count, setCount] = useState;
+
+    function increment(){
+        setCount(c => c + 1);
+        setCount(c => c + 1);
+        setCount(c => c + 1);
+
+    }
+}
+export default Updater
