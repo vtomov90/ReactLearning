0365df016a3cd6067f709152f2575380845825ed
Vasil <tomov.tomov09@gmail.com>
2025-04-08 00:33:03 +0200

    nwe feature added

---
+++ my-react-app/src/ComponentA.jsx
@@ -0,0 +1,20 @@
+import React, {useState, createContext} from 'react';
+import ComponentB from "./ComponentB.jsx"
+
+export const UserContext = createContext()
+function ComponentA(){
+
+    const [user, setUser] = useState("Vasil");
+
+    return(
+        <div>
+            <h1>ComponentA</h1>
+            <h2>{`Hello ${user}`}</h2>
+            <UserContext.Provider value={user}>
+                <ComponentB user={user}/>
+            </UserContext.Provider>
+            
+        </div>
+    )
+}
+export default ComponentA
